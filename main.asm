12a9c92d5654d131c6698d7f8e9aa3ec9a32974a
JoseSLK <jose.salamanca05@uptc.edu.co>
2026-03-28 19:01:45 -0500

    fix: Print strings function

--- main.asm
+++ main.asm
@@ -1,17 +1,17 @@
 ; Heyyyy
 org 100h        
    ;Imprimo los nombre primero (Flujo Principal)
-   mov dx, offset nicolas
-   call PrintStringDOS                                      
+   mov si, offset nicolas
+   call PrintString                                    
    
-   mov dx, offset jose
-   call PrintStringDOS         
+   mov si, offset jose
+   call PrintString        
    
    ; Aca empieza el bucle del menu principal
 continuePrincipal:
     ; Imprimimos el menu
-   mov dx, offset menu
-   call PrintStringDOS  
+   mov si, offset menu
+   call PrintString 
    ; Esperamos al entrada
    mov ah, 0
    int 16h
@@ -35,25 +35,53 @@ exitProgram:
    
 ret    
 ; Algunas variables
-nicolas db "Nicolas Tinjaca 202210716",13,10,"$"    
-jose db "Jose Salamanca 202214283",13,10,"$"
-menu db "Opt: 1:Calc 2:ART 3:Byeee",13,10,"$"
+nicolas db "Nicolas Tinjaca 202210716",13,10,0    
+jose db "Jose Salamanca 202214283",13,10,0
+menu db "Opt: 1:Calc 2:ART 3:Byeee",13,10,0 
+menuOperation db "Opt: Sum:s Subtract:r Multiplication:m",13,10,0
 
 ; Algunas funciones utiles
 PrintCharacter:
     mov ah, 0eh
     int 10h
 ret
-PrintStringDOS:
-    mov ah, 09h
-    int 21h
+PrintString:
+    mov ah, 0eh
+    stringLoop:    
+        mov al, [si]
+        cmp al, 0
+        je finLoop
+        int 10h
+        inc si
+        jmp stringLoop
+    finLoop: 
 ret
 
 ; La calculadora Humilde
-calFunction:           
-    mov dx, offset jose
-    call PrintStringDOS    
-    jmp continuePrincipal
+calFunction:
+    continueOperations:           
+        mov si, offset menuOperation
+        call PrintString
+        ;Las opciones de la calculadora
+        mov ah, 0
+        int 16h
+        ;Lo que se debe ejecutar
+        cmp al, 's'
+        je Sums
+        cmp al, 'r'
+        je Subtractions
+        cmp al, 'm'    
+        je Multiplications
+        cmp al, 'x'
+        je continuePrincipal
+        
+        jmp continueOperations
+       
+     Sums:
+     Subtractions:
+     Multiplications:
+        
+        
 
 ART:
     ;Su codigo
